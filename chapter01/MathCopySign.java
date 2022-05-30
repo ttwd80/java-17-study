0f6e1e65db216abe92cfc6b9559b27abff117c0c
Titi Wangsa bin Damhore <ttwd80@gmail.com>
2022-05-30 19:31:45 +1000

    -0 != -0.0

--- chapter01/MathCopySign.java
+++ chapter01/MathCopySign.java
@@ -5,13 +5,24 @@ public class MathCopySign {
         double[][] values = new double[][] {
                 { 11.2, 10.3 },
                 { 11.2, -2.2 },
-                { 11.2, 0 },
+                { 11.2, 0.00 },
+                { 11.2, -0.00 },
                 { -108.9, 10.3 },
                 { -108.9, -7.9 },
                 { -108.9, 0 },
+                { -108.9, -0 },
                 { 0, 9.21 },
                 { 0, -500.99 },
                 { 0, 0 },
+                { 0, -0 },
+                { -0, 200.22 },
+                { -0, -100.11 },
+                { -0, 0 },
+                { -0, -0 },
+                { -0.00, 1 },
+                { -0.00, -1 },
+                { -0.00, 0.00 },
+                { -0.00, -0.00 },
         };
         for (double[] value : values) {
             process(value[0], value[1]);
@@ -21,5 +32,6 @@ public class MathCopySign {
     private static void process(double magnitude, double sign) {
         System.out.println("Math.copySign(" + magnitude + ", " + sign + ")");
         System.out.println(Math.copySign(magnitude, sign));
+        System.out.println();
     }
 }
