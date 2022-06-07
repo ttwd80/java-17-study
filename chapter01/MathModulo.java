b19ced177515bff0cbb42b4dd2d3ddf719a1ec48
Titi Wangsa bin Damhore <ttwd80@gmail.com>
2022-06-07 14:23:09 +1000

    Update MathModulo.java

--- chapter01/MathModulo.java
+++ chapter01/MathModulo.java
@@ -18,34 +18,50 @@ public class MathModulo {
         System.out.println();
 
         System.out.println("All negative");
-        //-38 % -10
-        //throw away the sign
-        //38 % 10 = 8
-        //use the sign from -38 (negative)
-        //-8
+        // -38 % -10
+        // throw away the sign
+        // 38 % 10 = 8
+        // use the sign from -38 (negative)
+        // -8
         mathFloorMod(-38, -10);
         System.out.println();
 
-        //first get the non floor divide result
-        //31 / -10 = -3.1
-        //we floor -3.1
-        //-4.0 ... -3.1 ... -3.0
-        //the floor is -4
-        //-4 x -10 is 40
-        //our result is 38 - 40, which is -2
+        // first get the non floor divide result
+        // 31 / -10 = -3.1
+        // we floor -3.1
+        // -4.0 ... -3.1 ... -3.0
+        // the floor is -4
+        // -4 x -10 is 40
+        // our result is 38 - 40, which is -2
         System.out.println("Math.floorMod(positive, negative)");
         mathFloorMod(38, -10);
         System.out.println();
 
-        //first get the non floor divide result
-        //-31 / 10 = -3.1
-        //we floor -3.1
-        //-4.0 ... -3.1 ... -3.0
-        //the floor is -4
-        //-4 x 10 is -40
-        //our result is (-38) - (-40), which is 2
+        // first get the non floor divide result
+        // -31 / 10 = -3.1
+        // we floor -3.1
+        // -4.0 ... -3.1 ... -3.0
+        // the floor is -4
+        // -4 x 10 is -40
+        // our result is (-38) - (-40), which is 2
         System.out.println("Math.floorMod(negative, positive)");
         mathFloorMod(-38, 10);
+        System.out.println();
+
+        // zero with positive, would get 0
+        mathFloorMod(0, 10);
+        System.out.println();
+
+        // zero with negative, would get 0
+        mathFloorMod(0, -10);
+        System.out.println();
+
+        // ArithmeticException: / by zero
+        mathFloorMod(10, 0);
+        System.out.println();
+
+        // ArithmeticException: / by zero
+        mathFloorMod(-10, 0);
         System.out.println();
 
     }
@@ -90,9 +106,27 @@ public class MathModulo {
         // since we have -38, the answer is also negative, -8
         // the negative in -10 can be ignored
         normalModulo(-38, -10);
+
+        // zero with positive, would get 0
+        normalModulo(0, 10);
+
+        // zero with negative, would get 0
+        normalModulo(0, -10);
+
+        // ArithmeticException: / by zero
+        normalModulo(10, 0);
+
+        // ArithmeticException: / by zero
+        normalModulo(-10, 0);
     }
 
     private static void normalModulo(int x, int y) {
-        System.out.println(x + " % " + y + " = " + (x % y));
+        try {
+            System.out.print(x + " % " + y + " = ");
+            System.out.println(x % y);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
